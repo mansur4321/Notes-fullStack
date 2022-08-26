d879b730c0f0e92fffc906edb9cb90a8b4255420
mansur4321 <jet_011@mail.ru>
2022-08-26 21:58:20 +0300

    note-min
    сделал маленькие карточки с заметками

--- notes-fullStack-app/components/app-note/min-note-c.vue
+++ notes-fullStack-app/components/app-note/min-note-c.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="note-min">
     <div class="note-min__left-wrapper">
-      <span class="note-min__text">----------</span>
+      <span class="note-min__text">name note</span>
 
       <span class="note-min__date">12.05</span>
     </div>
 
     <div class="note-min__right-wrapper">
-      <a class="note-min__btn-del">
+      <a class="note-min__btn">
+        <img src="../../static/icon-delete-bin-blue.png" class="note-min__image-blue">
         <img src="../../static/icon-delete-bin.png" class="note-min__image">
       </a>
 
-      <a class="note-min__btn-fix">
+      <a class="note-min__btn">
+        <img src="../../static/icon-office-push-pin-blue.png" class="note-min__image-blue">
         <img src="../../static/icon-office-push-pin.png" class="note-min__image">
       </a>
     </div>
